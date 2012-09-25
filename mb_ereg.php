e1892efc82afb1a15b8ddbd60d3c900deef75a87
kakikubo teruo <kakikubo@gmail.com>
2012-09-25 17:55:04 +0900

    メモ書き追記

--- mb_ereg.php
+++ mb_ereg.php
@@ -4,4 +4,4 @@ $test = 'パーフェクトPHP';
 $pattern = 'パーフェクト';
 $result = mb_ereg($pattern, $test);
 var_dump($result);
-echo mb_regex_encoding(), PHP_EOL;
+echo mb_regex_encoding(), PHP_EOL; // この関数で返ってくる、もしくは設定される文字コードか、内部エンコーディングが使用される。
